e2d9dcac31e471b7b773c78f67d14f16dc27173c
kulanov.av <kulanov.av@phystech.edu>
2023-10-19 00:42:19 +0300

    init

---
+++ src/NumDerivative.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void helloTest() {
+    std::cout << "Test" << std::endl;
+}
---
+++ tests/NumDerivativeTests.cc
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "../src/NumDerivative.hpp"
+
+TEST(TestSuite1, Test1) {
+    ASSERT_NO_FATAL_FAILURE(helloTest());
+}
