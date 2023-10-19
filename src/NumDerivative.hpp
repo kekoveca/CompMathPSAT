fab60e7ad90557afcd9157b773e6d8ca530cacbc
kekoveca <kulanov.av@phystech.edu>
2023-10-19 20:56:22 +0300

    eigen ready

--- src/NumDerivative.hpp
+++ src/NumDerivative.hpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <Eigen/Dense>
 
-void helloTest() {
-    std::cout << "Test" << std::endl;
+template<typename RealType, unsigned int N>
+struct DerivativeCoef {
+    RealType centralCoef;
+    std::array<RealType, N> otherCoefs;
+};
+
+template<typename RealType, unsigned int N>
+DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {
+    return 0;
 }
--- tests/NumDerivativeTests.cc
+++ tests/NumDerivativeTests.cc
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include "../src/NumDerivative.hpp"
+#include "../src/SlaeSolver.hpp"
+
 
 TEST(TestSuite1, Test1) {
-    ASSERT_NO_FATAL_FAILURE(helloTest());
+    std::vector<std::vector<float>> A(2, std::vector<float>(2));
+    ASSERT_NO_FATAL_FAILURE(t_f());
 }
