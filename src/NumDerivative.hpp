ac235509789b1f9d423645abafacab005af09f2e
kulanov.av <kulanov.av@phystech.edu>
2023-10-20 17:50:37 +0300

    a0.11

--- src/NumDerivative.hpp
+++ src/NumDerivative.hpp
@@ -2,40 +2,49 @@
 #include <Eigen/Dense>
 #include <cmath>
 
-template<typename RealType, unsigned int N>
+template<typename RealType, std::size_t N>
 struct DerivativeCoef {
     RealType centralCoef;
     std::array<RealType, N> otherCoefs;
 };
 
-template<typename RealType, unsigned int N>
-DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {
-    const unsigned int numberOfEquations = points.size() + 1;
-    Eigen::Array<RealType, numberOfEquations, 1> b;
+template<typename RealType, std::size_t N>
+// DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {
+Eigen::VectorXf calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {  
+    Eigen::Vector<RealType, N + 1> b;
     b.setZero();
     b(1) = 1;
 
-    Eigen::Matrix<RealType, numberOfEquations, numberOfEquations> A;
-    Eigen::Array<RealType, 1, numberOfEquations> tmp;
+    Eigen::Matrix<RealType, N + 1, N + 1> A;
+    Eigen::Array<RealType, 1, N + 1> tmp;
     tmp.setOnes();
-    A(0) = tmp;
+    A.row(0) = tmp;
 
     auto pointsCopy = points;
 
-    for(unsigned int j = 1; j < numberOfEquations; j++) {
+    for(unsigned int j = 1; j < N + 1; j++) {
         A(j, 0) = 0;
-        for(unsigned int i = 1; i < numberOfEquations; i++) {
+        for(unsigned int i = 1; i < N + 1; i++) {
             A(j, i) = std::pow(pointsCopy[i - 1], j);
         }
     }
 
-    std::array<RealType, N + 1> allCoefs = A.colPivHouseholderQr().solve(b);
+    typedef Matrix<RealType, N, Dynamic
+
+    Eigen::ColPivHouseholderQR<Eigen::MatrixXf> dec(A);
+    Eigen::VectorXf allCoefs = dec.solve(b);
 
     DerivativeCoef<RealType, N> ans;
-    ans.centralCoef = allCoefs[1];
-    for(unsigned int i = 0; i != 1, i < N + 1; i++) {
+
+    for(unsigned int i = 0; i < 1; i++) {
         ans.otherCoefs[i] = allCoefs[i];
     }
 
-    return ans;
+    ans.centralCoef = allCoefs(1);
+    
+    for(unsigned int i = 2; i < N + 1; i++) {
+        ans.otherCoefs[i - 1] = allCoefs[i];
+    }
+
+    return {};
 }
--- tests/NumDerivativeTests.cc
+++ tests/NumDerivativeTests.cc
@@ -1,10 +1,11 @@
 #include <gtest/gtest.h>
 #include "../src/NumDerivative.hpp"
+#include "../src/test.hpp"
 
 
 TEST(NumDerivativeTests, FirstTest){
-    const unsigned int N = 2;
-    const std::array<double, 2> points = {-2, 3};
-    DerivativeCoef<double, 2> ans = calcDerivativeCoef(points);
-    ASSERT_NO_FATAL_FAILURE();
+    const long N = 2;
+    const std::array<long, 2> points = {-1, 1};
+    // calcDerivativeCoef(points);
+    test_eigen();
 }
