02ee633d8527722aaafe8a4c4b2b6c116045898b
kekoveca <kulanov.av@phystech.edu>
2023-10-24 01:49:18 +0300

    move

--- src/NumDerivative.hpp
+++ src/NumDerivative.hpp
@@ -1,50 +1,42 @@
-#include <iostream>
 #include <Eigen/Dense>
 #include <cmath>
 
-template<typename RealType, std::size_t N>
-struct DerivativeCoef {
-    RealType centralCoef;
-    std::array<RealType, N> otherCoefs;
-};
+// template<typename RealType, std::size_t N>
+// struct DerivativeCoef {
+//     RealType centralCoef;
+//     std::array<RealType, N> otherCoefs;
+// };
 
 template<typename RealType, std::size_t N>
-// DerivativeCoef<RealType, N> calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {
-Eigen::VectorXf calcDerivativeCoef(const std::array<RealType, N>& points) noexcept {  
-    Eigen::Vector<RealType, N + 1> b;
+auto calcDerivativeCoef(const std::array<RealType, N>& points) {
+
+    Eigen::Matrix<RealType, N + 1, 1> b;
     b.setZero();
     b(1) = 1;
 
     Eigen::Matrix<RealType, N + 1, N + 1> A;
-    Eigen::Array<RealType, 1, N + 1> tmp;
+    Eigen::Matrix<RealType, 1, N + 1> tmp;
     tmp.setOnes();
     A.row(0) = tmp;
-
-    auto pointsCopy = points;
 
     for(unsigned int j = 1; j < N + 1; j++) {
         A(j, 0) = 0;
         for(unsigned int i = 1; i < N + 1; i++) {
-            A(j, i) = std::pow(pointsCopy[i - 1], j);
+            A(j, i) = std::pow(points[i - 1], j);
         }
     }
 
-    typedef Matrix<RealType, N, Dynamic
+    typedef Eigen::Matrix<RealType, Eigen::Dynamic, Eigen::Dynamic> RealMatrix;
+    typedef Eigen::Vector<RealType, N + 1> AnswArray;
 
-    Eigen::ColPivHouseholderQR<Eigen::MatrixXf> dec(A);
-    Eigen::VectorXf allCoefs = dec.solve(b);
+    Eigen::ColPivHouseholderQR<RealMatrix> dec(A);
+    AnswArray answerTemporary = dec.solve(b);
 
-    DerivativeCoef<RealType, N> ans;
-
-    for(unsigned int i = 0; i < 1; i++) {
-        ans.otherCoefs[i] = allCoefs[i];
-    }
-
-    ans.centralCoef = allCoefs(1);
+    std::array<RealType, N + 1> answer;
     
-    for(unsigned int i = 2; i < N + 1; i++) {
-        ans.otherCoefs[i - 1] = allCoefs[i];
+    for(unsigned int i = 0; i < N + 1; i++) {
+        answer[i] = answerTemporary[i];
     }
 
-    return {};
+    return answer;
 }
--- tests/NumDerivativeTests.cc
+++ tests/NumDerivativeTests.cc
@@ -1,11 +1,71 @@
 #include <gtest/gtest.h>
 #include "../src/NumDerivative.hpp"
-#include "../src/test.hpp"
+#include <iostream>
+#include <fstream>
 
+const std::string dataPath = "C:/Users/kekoveca/Documents/CompMathPSAT/post_proccessing";
 
-TEST(NumDerivativeTests, FirstTest){
-    const long N = 2;
-    const std::array<long, 2> points = {-1, 1};
-    // calcDerivativeCoef(points);
-    test_eigen();
+TEST(NumDerivativeTests, main){
+    std::ofstream fileOut;
+    fileOut.open(dataPath + "/NumDerivativeFolder/errors.txt");
+
+    const unsigned int N = 2;
+    const std::array<float, N> points = {1, 2};
+
+    auto coeffs = calcDerivativeCoef(points);
+
+    constexpr double x = 1.;
+
+    constexpr unsigned int N3 = 3;
+    constexpr unsigned int N4 = 4;
+    constexpr unsigned int N5 = 5;
+    
+    std::array<double, N3> pointsN3 = {1., 2., 3.};
+    std::array<double, N4> pointsN4 = {1., 2., 3., 4.};
+    std::array<double, N5> pointsN5 = {1., 2., 3., 4., 5.};
+
+    std::array<double, 16> h{};
+
+    h[0] = 1;
+
+    for(unsigned int i = 0; i < h.size() - 1; i++) {
+        h[i + 1] = h[i] * 0.5;
+    }
+
+    auto coeffsN3 = calcDerivativeCoef(pointsN3);
+
+    const double exp0 = std::exp(x); 
+
+    for(auto&& iter : h) {
+        double DN3 = coeffsN3[0] * exp0;                        // A_0 * f(x_0)
+        for(unsigned int i = 0; i < pointsN3.size(); i++){
+            DN3 += coeffsN3[i + 1] * pointsN3[i];               // A_i * f(x_0 + ih)
+        }
+        fileOut<<std::abs(DN3 / iter - exp0)  <<  " ";
+    }
+    fileOut<<std::endl;
+
+    // DerivativeCoef<double, N4> answN4 = calcDerivativeCoef(pointsN4);
+    // //double DN4 = answN4.centralCoef * std::exp(x);
+
+    // for(auto&& it : h){
+    //     double DN4 = answN4.centralCoef * std::exp(x);
+    //     for(std::uint64_t j = 0; j < pointsN4.size(); j++){
+    //         DN4+=answN4.otherCoefs[j] * std::exp(x + pointsN4[j]*it);
+    //     }
+    //     fileOut<<std::abs(double(DN4)/it - double(std::exp(x)))<<" ";
+    // }
+    // fileOut<<std::endl;
+
+    // DerivativeCoef<double, N5> answN5 = calcDerivativeCoef(pointsN5);
+    // //double DN5 = answN5.centralCoef * std::exp(x);
+    // for(auto&& it : h){
+    //     double DN5 = answN5.centralCoef * std::exp(x);
+    //     for(std::uint64_t j = 0; j < pointsN5.size(); j++){
+    //         DN5+=answN5.otherCoefs[j] * std::exp(x + pointsN5[j]*it);
+    //     }
+    //     fileOut<<std::abs(DN5/it - std::exp(x))<<" ";
+    // }
+    fileOut<<std::endl;
+    fileOut.close();
 }
