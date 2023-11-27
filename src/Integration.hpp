83673b34022fa3d6a42a56a8e3639906cd2dd552
kulanov.av <kulanov.av@phystech.edu>
2023-11-27 11:22:11 +0300

    integration ready

--- src/Integration.hpp
+++ src/Integration.hpp
@@ -1,27 +1,57 @@
-#include <array>
+#include <vector>
 #include <type_traits>
+#include <algorithm>
+#include <map>
 
-constexpr std::array<double, 1> LEJENDRE_ZEROS_2    = {0.577350269189626};
-constexpr std::array<double, 1> LEJENDRE_WEIGHTS_2  = {1.};
+class ZerosAndWeights {
+    public:
+    std::vector<double> zeros_, weights_;
+    ZerosAndWeights(const std::vector<double>& zeros, const std::vector<double>& weights){
+        auto size = zeros_.size();
+        zeros_.resize(size);
+        weights_.resize(size);
+        zeros_ = zeros;
+        weights_ = weights;
+    };
+};
 
-constexpr std::array<double, 2> LEJENDRE_ZEROS_3    = {0., 0.774596669241483};
-constexpr std::array<double, 2> LEJENDRE_WEIHGTS_3  = {0.888888888888889, 0.55555555555555};
+const std::vector<double> LEJENDRE_ZEROS_2    = {0.577350269189626};
+const std::vector<double> LEJENDRE_WEIGHTS_2  = {1.};
 
-constexpr std::array<double, 2> LEJENDRE_ZEROS_4    = {0.339981043584856, 0.86113631159405};
-constexpr std::array<double, 2> LEJENDRE_WEIHGTS_4  = {0.652145154862546, 0.34785484513745};
+const std::vector<double> LEJENDRE_ZEROS_3    = {0., 0.774596669241483};
+const std::vector<double> LEJENDRE_WEIGHTS_3  = {0.888888888888889, 0.55555555555555};
 
-constexpr std::array<double, 3> LEJENDRE_ZEROS_5    = {0., 0.538469310105683, 0.90617984593866};
-constexpr std::array<double, 3> LEJENDRE_WEIHGTS_5  = {0.568888888888889, 0.478628670499366, 0.236926885056189};
+const std::vector<double> LEJENDRE_ZEROS_4    = {0.339981043584856, 0.86113631159405};
+const std::vector<double> LEJENDRE_WEIGHTS_4  = {0.652145154862546, 0.34785484513745};
 
-constexpr std::array<double, 3> LEJENDRE_ZEROS_6    = {0.238619186083197, 0.661209386466265, 0.932469514203152};
-constexpr std::array<double, 3> LEJENDRE_WEIHGTS_6  = {0.467913934572691, 0.360761573048139, 0.171324492379170};
+const std::vector<double> LEJENDRE_ZEROS_5    = {0., 0.538469310105683, 0.90617984593866};
+const std::vector<double> LEJENDRE_WEIGHTS_5  = {0.568888888888889, 0.478628670499366, 0.236926885056189};
 
-constexpr std::array<double, 8> LEJENDRE_ZEROS_15   = {0., 0.201194093997435, 0.394151347077563, 0.570972172608539, 
+const std::vector<double> LEJENDRE_ZEROS_6    = {0.238619186083197, 0.661209386466265, 0.932469514203152};
+const std::vector<double> LEJENDRE_WEIGHTS_6  = {0.467913934572691, 0.360761573048139, 0.171324492379170};
+
+const std::vector<double> LEJENDRE_ZEROS_15   = {0., 0.201194093997435, 0.394151347077563, 0.570972172608539, 
                                                        0.724417731360170, 0.848206583410427, 0.937273392400706, 0.987992518020485};
-constexpr std::array<double, 8> LEJENDRE_WEIHGTS_15 = {0.202578241925561, 0.198431485327111, 0.186161000015562, 0.166269205816994,
+const std::vector<double> LEJENDRE_WEIGHTS_15 = {0.202578241925561, 0.198431485327111, 0.186161000015562, 0.166269205816994,
                                                        0.139570677926154, 0.107159220467172, 0.070366047488108, 0.030753241996117};
 
-const std::array<std::size_t, 6> availableQuadratures= {2, 3, 4, 5, 6, 15};
+const auto Gauss2 = std::make_pair(LEJENDRE_ZEROS_2, LEJENDRE_WEIGHTS_2);
+const auto Gauss3 = std::make_pair(LEJENDRE_ZEROS_3, LEJENDRE_WEIGHTS_3);
+const auto Gauss4 = std::make_pair(LEJENDRE_ZEROS_4, LEJENDRE_WEIGHTS_4);
+const auto Gauss5 = std::make_pair(LEJENDRE_ZEROS_5, LEJENDRE_WEIGHTS_5);
+const auto Gauss6 = std::make_pair(LEJENDRE_ZEROS_6, LEJENDRE_WEIGHTS_6);
+const auto Gauss15 = std::make_pair(LEJENDRE_ZEROS_15, LEJENDRE_WEIGHTS_15);
+
+
+const std::map<std::size_t, std::pair<std::vector<double>, std::vector<double>>> availableQuadratures {
+    {2, Gauss2},
+    {3, Gauss3},
+    {4, Gauss4},
+    {5, Gauss5},
+    {6, Gauss6},
+    {15, Gauss15}
+};
+
 
 template<typename A>
 struct ArgumentGetter;
@@ -37,17 +67,56 @@ using Dif = decltype(std::declval<T>() - std::declval<T>());
 /* Функция производит интегрирование на одном отрезке */
 template<typename Callable, std::size_t N>
 decltype(auto) integrate(const Callable& func, const typename ArgumentGetter<Callable>::Argument& start, const typename ArgumentGetter<Callable>::Argument& end){
-
+    typename ArgumentGetter<Callable>::Argument ans = 0;
+    const auto quadrature = availableQuadratures.at(N);
+    const auto delta = end - start;
+    if(N % 2 != 0) {
+        ans += (delta) / 2 * func((end + start) / 2 + quadrature.first[0] * (delta) / 2) * quadrature.second[0];
+        for(std::size_t i = 1; i < N / 2 + 1; i++) {
+            ans += (delta) / 2 * func((end + start) / 2 + quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+            ans += (delta) / 2 * func((end + start) / 2 - quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+        }
+    }
+    else {
+        for(std::size_t i = 0; i < N / 2; i++) {
+            ans += (delta) / 2 * func((end + start) / 2 + quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+            ans += (delta) / 2 * func((end + start) / 2 - quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+        }    
+    }
+    return ans;
 }
 
 
-
-
-// /* Функция производит интегрирование, разбивая отрезок на подотрезки длиной не более dx */
-// template<typename Callable, std::size_t N>
-// decltype(auto) integrate(   
-//     const Callable& func,  // Интегрируемая функция
-//     const typename ArgumentGetter<Callable>::Argument& start,  // начало отрезка
-//     const typename ArgumentGetter<Callable>::Argument& end,  // конец отрезка
-//     const Dif<typename ArgumentGetter<Callable>::Argument>& dx  // Длина подотрезка
-//                         );
+/* Функция производит интегрирование, разбивая отрезок на подотрезки длиной не более dx */
+template<typename Callable, std::size_t N>
+decltype(auto) integrate(const Callable& func, const typename ArgumentGetter<Callable>::Argument& start, const typename ArgumentGetter<Callable>::Argument& end, const Dif<typename ArgumentGetter<Callable>::Argument>& dx) {
+    typename ArgumentGetter<Callable>::Argument ans = 0;
+    const auto quadrature = availableQuadratures.at(N);
+    const auto Delta = end - start;
+    const unsigned int numberOfIntervals = std::ceil(Delta / dx);
+    const Dif<typename ArgumentGetter<Callable>::Argument> exactDx = Delta / numberOfIntervals;
+    Dif<typename ArgumentGetter<Callable>::Argument> currStart = start;
+    Dif<typename ArgumentGetter<Callable>::Argument> currEnd = start + exactDx;
+    for(unsigned int j = 0; j < numberOfIntervals; j++) {
+        if(N % 2 != 0) {
+            auto delta = currEnd - currStart;
+            ans += (delta) / 2 * func((currEnd + currStart) / 2 + quadrature.first[0] * (delta) / 2) * quadrature.second[0];
+            for(std::size_t i = 1; i < N / 2 + 1; i++) {
+                ans += (delta) / 2 * func((currEnd + currStart) / 2 + quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+                ans += (delta) / 2 * func((currEnd + currStart) / 2 - quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+            }
+            currStart += exactDx;
+            currEnd += exactDx;
+        }
+        else {
+            auto delta = currEnd - currStart;
+            for(std::size_t i = 0; i < N / 2; i++) {
+                ans += (delta) / 2 * func((currEnd + currStart) / 2 + quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+                ans += (delta) / 2 * func((currEnd + currStart) / 2 - quadrature.first[i] * (delta) / 2) * quadrature.second[i];
+            }
+            currStart += exactDx;
+            currEnd += exactDx;  
+        }
+    }
+    return ans;
+}
--- tests/IntegrationTests.cc
+++ tests/IntegrationTests.cc
@@ -5,6 +5,48 @@
 
 const std::string dataPath = "/Users/kekoveca/Documents/MIPT/CompMath/post_proccessing/IntegrationFolder";
 
-TEST(IntegrationTests, BasicTest) {
-    ArgumentGetter<double(double)> x;
+TEST(IntegrationTests, basicTest) {
+    double answer = 1. - std::cos(double(10.));
+    constexpr double start = 0;
+    constexpr double end = 10;
+    constexpr std::array<std::size_t, 6> nums = {2, 3, 4, 5, 6, 15};
+    std::cout << std::abs(answer - integrate<double(double), 2>(std::sin, start, end)) << "\n";
+    std::cout << std::abs(answer - integrate<double(double), 3>(std::sin, start, end)) << "\n";
+    std::cout << std::abs(answer - integrate<double(double), 4>(std::sin, start, end)) << "\n";
+    std::cout << std::abs(answer - integrate<double(double), 5>(std::sin, start, end)) << "\n";
+    std::cout << std::abs(answer - integrate<double(double), 6>(std::sin, start, end)) << "\n";
+    std::cout << std::abs(answer - integrate<double(double), 15>(std::sin, start, end)) << "\n";
+}
+
+TEST(IntegrationTests, firstTest){
+    std::ofstream fileOut;
+    fileOut.open(dataPath + "/err.txt");
+    constexpr double start = 0;
+    constexpr double end = 10;
+    constexpr std::array dx{8., 4., 2., 1., 1./2., 1./4., 1./8., 1./16., 1./32., 1./64., 1./128., 1./256., 1./512., 1./1024.};
+    double answer = 1. - std::cos(double(10.));
+    for(auto&& it: dx){
+        fileOut << std::abs(answer - integrate<double(double), 2>(std::sin, start, end, it)) << ",";
+    }
+    fileOut << "\n";
+    for(auto&& it: dx){
+        fileOut << std::abs(answer - integrate<double(double), 3>(std::sin, start, end, it)) << ",";
+    }
+    fileOut << "\n";
+    for(auto&& it: dx){
+        fileOut << std::abs(answer - integrate<double(double), 4>(std::sin, start, end, it)) << ",";
+    }
+    fileOut << "\n";
+    for(auto&& it: dx){
+        fileOut << std::abs(answer - integrate<double(double), 5>(std::sin, start, end, it)) << ",";
+    }
+    fileOut << "\n";
+    for(auto&& it: dx){
+        fileOut << std::abs(answer - integrate<double(double), 6>(std::sin, start, end, it)) << ",";
+    }
+    fileOut << "\n";
+    for(auto&& it: dx){
+        fileOut << std::abs(answer - integrate<double(double), 15>(std::sin, start, end, it)) << ",";
+    }
+    fileOut.close();
 }
