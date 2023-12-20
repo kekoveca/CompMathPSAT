6881c7d0910642231a61136c0390c24cf3bd3e46
kekoveca <kulanov.av@phystech.edu>
2023-12-21 02:27:47 +0300

    rk4 ready

--- src/RungeKutta.hpp
+++ src/RungeKutta.hpp
@@ -13,6 +13,26 @@ struct RK4Table{
     static constexpr std::array<double, stages> bString = {1./6, 1./3, 1./3, 1./6};
 };
 
+class tCube{
+public:
+
+    static constexpr unsigned int dim = 1;  // размерность задачи
+
+    using Argument = double;  // тип аргумента, тип t
+
+    using State = Eigen::Vector<double, dim>;  // состояние
+
+    struct StateAndArg {
+        State state;
+        Argument arg;
+    };
+
+    /*** Вычисляет правую часть ДУ - функцию f***/
+    Eigen::Vector<double, dim> calc(const StateAndArg& stateAndArg) const {
+        return Eigen::Vector<double, dim>{std::pow(stateAndArg.arg, 3)};
+    }
+};
+
 class Oscillator {
 
 public:
@@ -34,38 +54,49 @@ public:
     } 
 };
 
-template<typename Table, typename RHS>  // таблица бутчера и класс правой части f
-std::vector<typename RHS::StateAndArg> integrate(
-    const typename RHS::StateAndArg& initialState,
-    const typename RHS::Argument& endTime,
-    double step,
-    const RHS& rhs
-) {
-    static constexpr unsigned int dim = rhs.dim;
-    rhs.StateAndArg = initialState;
-    auto S = Table::stages;
-    std::array<Eigen::Vector<double, dim>, S> arrayOfK;
-    const Eigen::Vector<double, dim> k1 = rhs.calc(initialState);
-    typename RHS::StateAndArg currentTimeState = initialState;
-
-    while(currentTimeState.arg < endTime) {
-        arrayOfK[0] = k1;
-        auto currentState = currentTimeState;
-        for(unsigned int i = 1; i < S; i++) {
-            double sumAK = 0;
-            for(unsigned int j = 0; j < i - 1; j++) {
-                sumAK += Table::table[i][j] * arrayOfK[j];
-            }
-            currentState = {currentTimeState.state + step * sumAK, currentTimeState.arg + step * Table::cColumn[i]};
-            arrayOfK[i] = rhs.calc(currentState);
-        }
-        double sumBK = 0;
-        for(unsigned int i = 0; i < S; i++) {
-            sumBK += Table::bString[i] * arrayOfK[i];
-        }
-        currentTimeState.state = currentTimeState.state + step * sumBK;
-        currentTimeState.arg += step;
+template<typename aType, typename bType, std::size_t N>
+decltype(auto) dot(const std::array<aType, N> &a, const std::array<bType, N> &b) {
+    bType ans = a[0] * b[0];
+    for (std::size_t i = 1; i < a.size(); i++) {
+        ans += a[i] * b[i];
     }
+    return ans;
+}
 
-    return currentTimeState;
+
+template<typename Table, typename RHS>
+std::vector<typename RHS::StateAndArg> integrate(
+        const typename RHS::StateAndArg &initialState,
+        const typename RHS::Argument &endTime,
+        double step,
+        const RHS &rhs
+) {
+    using uType = typename RHS::StateAndArg;
+    using fType = Eigen::Vector<double, RHS::dim>;
+    using rType = double;
+    uType uCurrent = initialState;
+    std::vector<uType> uVector{uCurrent};
+    std::size_t numStep = endTime / step;
+    uVector.reserve(numStep);
+
+    for (std::size_t i = 1; i <= numStep; i++) {
+        std::array<fType, Table::stages> k{};
+        k[0] = rhs.calc(uCurrent);
+        for(unsigned int i = 1; i < Table::stages; i++) {
+            k[i] = rhs.calc({uCurrent.state + step * dot<rType, fType, Table::stages>(Table::table[i], k), uCurrent.arg + step * Table::cColumn[i]});
+        }
+        uCurrent = {uCurrent.state + step * dot<rType, fType, Table::stages>(Table::bString, k), uCurrent.arg + step};
+        uVector.push_back(uCurrent);
+    }
+    
+    if(endTime - step * double(numStep) > 0) {
+        std::array<fType, Table::stages> k{};
+        k[0] = rhs.calc(uCurrent);
+        for(unsigned int i = 1; i < Table::stages; i++) {
+            k[i] = rhs.calc({uCurrent.state + step * dot<rType, fType, Table::stages>(Table::table[i], k), uCurrent.arg + step * Table::cColumn[i]});
+        }
+        uCurrent = {uCurrent.state + step * dot<rType, fType, Table::stages>(Table::bString, k), uCurrent.arg + step};
+        uVector.push_back(uCurrent);
+    }
+    return uVector;
 }
--- tests/RungeKuttaTests.cc
+++ tests/RungeKuttaTests.cc
@@ -3,13 +3,60 @@
 #include <iostream>
 #include <fstream>
 #include <array>
-#include <Eigen/Dense>
 
-const std::string dataPath = "/Users/kekoveca/Documents/MIPT/CompMath/post_proccessing/RungeKuttaFolder";
+const std::string dataPath = "C:\\Users\\kekoveca\\Documents\\CompMathPSAT\\post_proccessing\\RungeKuttaFolder";
 
-TEST(RungeKuttaTests, oscillatorTest) {
-    typename Oscillator::Argument initArg = 0;
-    typename Oscillator::State initState = {1., 0.};
-    typename Oscillator::StateAndArg initStateAndArg = {initState, initArg};
-    auto ans = integrate<RK4Table, Oscillator>()
+
+TEST(RungeKuttaTests, mainFirstTest){
+    std::ofstream errFile;
+    errFile.open(dataPath + "/err.txt");
+    double endTime = 5.;
+
+    auto func = [](auto x){return std::pow(x, 4) / 4.;};
+    double step = 2;
+    for(std::size_t i = 0; i <= 12; i++){
+        step = step * 0.5;
+        int stepNumber = endTime / step;
+        std::vector<double> resultTrue;
+        double err = 0;
+        for(std::size_t j = 0; j <= stepNumber; j++){
+            resultTrue.push_back(func(step * j));
+        }
+        resultTrue.push_back(func(endTime));
+        auto result = integrate<RK4Table, tCube>(tCube::StateAndArg{tCube::State {0.}, 0}, endTime, step, tCube{});
+        for(std::size_t j = 0; j < result.size(); j++){
+
+            err = std::max(err, std::abs((result[j].state(0) - resultTrue[j])));
+        }
+        errFile << err << ",";
+    }
+    errFile.close();
+}
+
+TEST(RungeKuttaTests, mainSecondTest){
+    std::ofstream errFile;
+    errFile.open(dataPath + "/err1.txt");
+    double endTime = 5.;
+
+    auto func = [](auto x){return std::sin(x);};
+    double step = 2;
+    for(std::size_t i = 0; i <= 12; i++){
+        step = step * 0.5;
+        int stepNumber = endTime / step;
+        std::vector<double> resultTrue;
+        double err = 0;
+        for(std::size_t j = 0; j <= stepNumber; j++){
+            resultTrue.push_back(func(step * j));
+        }
+
+        resultTrue.push_back(func(endTime));
+
+        auto result = integrate<RK4Table, Oscillator>(Oscillator::StateAndArg{Oscillator::State{0., 1.}, 0.}, endTime, step, Oscillator{});
+
+        for(std::size_t j = 0; j < result.size(); j++){
+            err = std::max(err, std::abs((result[j].state(0) - resultTrue[j])));
+        }
+        errFile << err << ",";
+    }
+    errFile.close();
 }
